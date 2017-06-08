8f41517f33f3241825361ecdf1441e9be3f5463f
jrbeverly <jrbeverly@live.com>
2017-06-08 18:26:34 -0400

    Reviewing the provision scripts

--- src/provision/install.sh
+++ src/provision/install.sh
@@ -2,19 +2,19 @@
 set -x
 cd /tmp/
 
+#  
 # Variables
 #
-# Variables related to the glibc package.
 
 ALPINE_GLIBC_BASE_PACKAGE_FILENAME="glibc-$ALPINE_GLIBC_PACKAGE_VERSION.apk"
 ALPINE_GLIBC_BIN_PACKAGE_FILENAME="glibc-bin-$ALPINE_GLIBC_PACKAGE_VERSION.apk"
 ALPINE_GLIBC_I18N_PACKAGE_FILENAME="glibc-i18n-$ALPINE_GLIBC_PACKAGE_VERSION.apk"
 
+# 
 # Install
 #
-# Install libraries necessary for the running of the application.
 
-apk add --no-cache --virtual=.build-dependencies wget ca-certificates
+apk add --update --no-cache --virtual=build-dependencies wget ca-certificates
 wget $ALPINE_GLIBC_KEY -O "/etc/apk/keys/sgerrand.rsa.pub"
 
 wget "$ALPINE_GLIBC_BASE_URL/$ALPINE_GLIBC_PACKAGE_VERSION/$ALPINE_GLIBC_BASE_PACKAGE_FILENAME" \
@@ -27,6 +27,6 @@ rm "/etc/apk/keys/sgerrand.rsa.pub"
 /usr/glibc-compat/bin/localedef --force --inputfile POSIX --charmap UTF-8 C.UTF-8 || true 
 echo "export LANG=C.UTF-8" > /etc/profile.d/locale.sh 
 
-apk del glibc-i18n .build-dependencies
-rm "$ALPINE_GLIBC_BASE_PACKAGE_FILENAME" "$ALPINE_GLIBC_BIN_PACKAGE_FILENAME" "$ALPINE_GLIBC_I18N_PACKAGE_FILENAME"
+apk del glibc-i18n build-dependencies
+rm -f "$ALPINE_GLIBC_BASE_PACKAGE_FILENAME" "$ALPINE_GLIBC_BIN_PACKAGE_FILENAME" "$ALPINE_GLIBC_I18N_PACKAGE_FILENAME"
 rm -rf /tmp/* /var/tmp/* /var/cache/apk/* /root/*
